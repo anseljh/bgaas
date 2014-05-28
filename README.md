c1db0b5fcfcae79ad86a3ac126708523e907f41e
Ansel Halliburton <a@anseljh.com>
2014-05-28 00:25:13 -0700

    Now with software! Basic command line option parser works; `update` command downloads a copy of the Consolidated Screening List. Added a requirements.txt file for dependencies. Lots of additions to README.md.

--- README.md
+++ README.md
@@ -1,15 +1,77 @@
 bgaas: Bad Guys As A Service
 ============================
 
-Bad Guys As a Service is software for querying export restrictions like
+**Bad Guys As A Service** (`bgaas`) is software for querying export restrictions like
 the Denied Persons List.
+
+Installation
+------------
+Install dependencies:
+```shell
+$ pip install -r src/requirements.txt
+```
+
+Run:
+```shell
+$ python src/bgaas.py <options> <command> <parameters>
+```
+Usage
+-----
+*Note: Most of this is not yet implemented!*
+
+### Update Lists
+
+```shell
+$ bgaas update
+```
+
+### Query
+
+```shell
+$ bgaas query agnese
+```
+returns results for the query "agnese" from all included lists.
+
+### Map
+```shell
+$ bgaas map agnese
+```
+
+Pronunciation
+-------------
+The pronunciation of `bgaas` is user selectable: either "bee gas" or "big a**".
 
 License
 -------
-bgaas's software and documentation are licensed under the
+`bgaas` software and documentation are licensed under the
 [Apache License](LICENSE). We make no claim to any data provided by
-governments.
+governments. Some government sample data is included, for testing purposes.
 
 Software
 --------
-bgaas is written in Python 3.
+`bgaas` is written in [Python](https://www.python.org/).
+
+Contributing
+------------
+Pull requests, feature ideas, and bug reports are welcome. You must agree to
+license any submitted code under the Apache License or it will be rejected.
+
+The repository is on GitHub at: https://github.com/anseljh/bgaas
+
+To-Do List
+----------
+* [ ] Parse Consolidated Screening List data file ([instructions](http://export.gov/static/cl_downloading_instructions_08102011_gh_Latest_eg_main_040971.pdf))
+* [ ] Document URLs of main raw list data
+* [ ] Document update frequency of each main list
+* [ ] Document command-line procedures for working with the main arms control [lists](docs/Lists.md) (e.g., with `grep`, `csvkit`)
+* [ ] Scrapers for debarment list
+    * [ ] Parse Excel file
+    * [ ] Parse HTML
+* [ ] Scraper for other US lists
+* [ ] Identify non-US lists
+* [ ] Mapping engine for plotting results, perhaps using [Kartograph](http://kartograph.org/) and [Natural Earth](http://www.naturalearthdata.com/)
+* [ ] Write installation instructions
+
+Further Reading
+---------------
+* [New Media Solutions in Nonproliferation and Arms Control: Opportunities and Challenges](http://www.nonproliferation.org/facebook-youtube-and-the-future-of-nonproliferation/) by Bryan Lee and Margarita Zolotova, published by [The James Martin Center for Nonproliferation Studies](http://www.nonproliferation.org/) (February 14, 2014)
--- src/bgaas.py
+++ src/bgaas.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2014 Ansel Halliburton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,8 +14,127 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import csv
-import optparse
+from __future__ import division
+
+import argparse
+import csvkit
+import requests
+import os
+import logging
+
+##############################################################################
+
+log = logging.getLogger('bgaas')
+log.setLevel(logging.DEBUG)
+# add a console handler
+# https://docs.python.org/3/howto/logging-cookbook.html#logging-cookbook
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+##############################################################################
+
+BGAAS_DIR = os.path.expanduser('~/.bgaas')
+BGAAS_DATA_DIR = BGAAS_DIR + os.sep + 'data'
+LISTS = ['csl', 'dpl', 'sdn', 'debar']
+LIST_NAMES = {
+    'csl': 'Consolidated Screening List',
+    'dpl': 'Denied Persons List',
+    'sdn': 'Specially Designated Nationals list',
+    'debar': 'AECA debarment list'
+}
+RAW_SOURCES = {
+    'csl': 'http://www.bis.doc.gov/export_consolidated_list/consolidated_party_list_final.csv',
+    'dpl': 'http://www.bis.doc.gov/dpl/dpl.txt',
+    'sdn': 'http://www.treasury.gov/ofac/downloads/sdn.csv', #TODO: other CSV files listed at http://www.treasury.gov/resource-center/sanctions/SDN-List/Pages/default.aspx
+    'debar': None #TODO: Parse XLS or HTML version
+    }
+
+##############################################################################
+
+def get_data_dir(args):
+    if args.data_dir:
+        log.debug("got data dir arg, setting to %s" % args.data_dir)
+        dir_name = args.data_dir
+    else:
+        dir_name = BGAAS_DATA_DIR
+
+    # check if exists; create if necessary
+    if not os.path.exists(BGAAS_DATA_DIR):
+        log.info("Creating directory: %s" % BGAAS_DATA_DIR)
+        os.makedirs(BGAAS_DATA_DIR)
+    return dir_name
+
+def do_update(args):
+    """Update arms control lists"""
+    # if args.verbosity > 0:
+    log.info("Verbosity: %d" % args.verbosity)
+    log.info("Data directory: %s" % get_data_dir(args))
+    log.info("Updating...")
+    csl = update_list(args, 'csl')
+    # if args.verbosity > 0:
+    log.info("Done.")
+    return True
+
+def update_list(args, list_):
+    # Check for 'all' as argument:
+    if list_ == 'all':
+        list_ = LISTS
+    # Convert list_ to a single-element list if it's just a string:
+    if type(list_) is str:
+        list_ = [list_]
+    if 'csl' in list_:
+        output_fn = get_data_dir(args) + os.sep + 'csl'
+        url = RAW_SOURCES['csl']
+        log.debug("Fetching: %s" % url)
+        r = requests.get(url)
+        size = int(r.headers.get('content-length'))
+        etag = r.headers.get('etag')
+        content_type = r.headers.get('content-type')
+        content = r.content
+        log.debug("Received %.2f MiB" % (size / 1024 / 1024))
+        assert content_type == 'text/plain', "content-type must be 'text/plain', but got '%s' instead." % content_type
+        log.debug("Writing output for CSL to %s" % output_fn)
+        output_f = open(output_fn, 'wb')
+        output_f.write(content)
+        output_f.close()
+    elif 'dpl' in list_:
+        raise Warning("DPL not implemented")
+    elif 'sdn' in list_:
+        raise Warning("SDN not implemented")
+    elif 'debar' in list_:
+        raise Warning("Debarment list not implemented")
+    else:
+        raise ValueError("invalid list name: %s" % list_)
+        return False
+
+def do_query(args):
+    log.debug("Recieved query with: %s" % args.Q)
+
+def do_map(args):
+    log.debug("Recieved map query with: %s" % args.Q)
+
+##############################################################################
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        prog='bgaas',
+        description='Bad Guys As A Service: Slice and dice arms control data for fun and profit.'
+        )
+    subparsers = parser.add_subparsers(title='subcommands')
+    parser.add_argument("-v", "--verbosity", action="count", help="increase output verbosity", default=0)
+    parser.add_argument("-l", "--list", help="use specified list", choices=['csl', 'dpl', 'sdn', 'debar'])
+    parser.add_argument("-d", "--data-dir", help="set data directory (defaults to %s)" % BGAAS_DATA_DIR, default=BGAAS_DATA_DIR)
+    update_parser = subparsers.add_parser('update', help='Update list data')
+    update_parser.set_defaults(func=do_update)
+    query_parser = subparsers.add_parser('query', help='Query lists', usage='bgaas query Q')
+    query_parser.add_argument('Q', help='The query term')
+    query_parser.set_defaults(func=do_query)
+    map_parser = subparsers.add_parser('map', help='Query and make a map', usage='bgaas map Q')
+    map_parser.add_argument('Q', help='The query term')
+    map_parser.set_defaults(func=do_map)
+
+    args = parser.parse_args()
+    args.func(args)
